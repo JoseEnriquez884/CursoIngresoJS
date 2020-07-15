0438abf48d840ab34e1ddbc07f8b0a2ff1f22e13
josebishop45 <bishop015@hotmail.com>
2020-07-15 12:17:37 -0300

    los primeros 5

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,8 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var nombreIngresado; //declaro variable
+	nombreIngresado = prompt("Ingrese su nombre", "Harry Potter"); //asigno o cargo la variable
+	alert("su nombre es "+nombreIngresado); //mostrar el valor de la variable
 }
 
