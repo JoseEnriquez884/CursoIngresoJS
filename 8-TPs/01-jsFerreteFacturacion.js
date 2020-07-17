88b65adf741c9b69080c4e331cc7a0eb34a34b5c
JoseEnriquez884 <bishop015@hotmail.com>
2020-07-16 22:06:10 -0300

    trabajo practico

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,70 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    //txtIdPrecioUno
+    //txtIdPrecioDos
+    //txtIdPrecioTres
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var sumaDePrecios;
+
+    precioUno=txtIdPrecioUno.value
+    precioUno=parseInt(precioUno);
+
+    precioDos=txtIdPrecioDos.value;
+    precioDos=parseInt(precioDos);
+
+    precioTres=txtIdPrecioTres.value;
+    precioTres=parseInt(precioTres);
+
+    sumaDePrecios=precioUno+precioDos+precioTres;
+
+    alert("La suma es: "+sumaDePrecios);
 }
 function Promedio () 
 {
-	
+	var precioUno;
+    var precioDos;
+    var precioTres;
+    var promedioDePrecios;
+
+    precioUno=txtIdPrecioUno.value
+    precioUno=parseInt(precioUno);
+
+    precioDos=txtIdPrecioDos.value;
+    precioDos=parseInt(precioDos);
+
+    precioTres=txtIdPrecioTres.value;
+    precioTres=parseInt(precioTres);
+
+    promedioDePrecios=(precioUno+precioDos+precioTres)/3;
+
+    alert("El promedio es: "+promedioDePrecios);
 }
 function PrecioFinal () 
 {
-	
+	var precioUno;
+    var precioDos;
+    var precioTres;
+    var sumaDePrecios;
+    var porcentajeDeIva;
+    //var precioFinal;
+
+    precioUno=txtIdPrecioUno.value
+    precioUno=parseInt(precioUno);
+
+    precioDos=txtIdPrecioDos.value;
+    precioDos=parseInt(precioDos);
+
+    precioTres=txtIdPrecioTres.value;
+    precioTres=parseInt(precioTres);
+
+    sumaDePrecios=precioUno+precioDos+precioTres;
+
+    porcentajeDeIva=sumaDePrecios*0.21;
+
+    precioFinal=sumaDePrecios+porcentajeDeIva;
+
+    alert("El precio final es: "+precioFinal);
 }
