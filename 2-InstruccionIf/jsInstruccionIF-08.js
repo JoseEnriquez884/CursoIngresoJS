77c1e2a227204a19b9b3f417237bb4d3a6e62b14
JoseEnriquez884 <bishop015@hotmail.com>
2020-07-27 01:34:07 -0300

    ponele que me salio

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,12 +1,12 @@
 //Enriquez Jose
 /*Se pide ingresar una edad y un estado civil
-	a todos los mayores de edad, mostrar el mensaje "se responsable"
-	a todos los menores de edad, mostrar el mensaje "respeta a tus mayores" 
-	a los adultos mayores( mas de 60 años) tambien informar "sos persona de riesgo"
-	a los niños tambien informar "hagan la tarea" 
-	a los adolescentes casado o Divorciado "sos muy joven para no ser soltero"
-	a los mayores de edad solteros, tambien informar "a vivir la vida"
-	a los mayores de edad casados , tambien informar "a disfrutar la pareja"
+	**a todos los mayores de edad, mostrar el mensaje "se responsable"
+	**a todos los menores de edad, mostrar el mensaje "respeta a tus mayores" 
+	**a los adultos mayores( mas de 60 años) tambien informar "sos persona de riesgo"
+	**a los niños tambien informar "hagan la tarea" 
+	**a los adolescentes casado o Divorciado "sos muy joven para no ser soltero"
+	**a los mayores de edad solteros, tambien informar "a vivir la vida"
+	**a los mayores de edad casados , tambien informar "a disfrutar la pareja"
 	a los divorciados informar tambien "a intentarlo nuevamente"
 
 */
@@ -24,19 +24,26 @@ function mostrar()
 
 	situacionSentimental=estadoCivil.value;
 
-	if(edadIngresada<18)
+	if(edadIngresada<18)//menores de edad
 	{
 		alert("respeta a tus mayores");
-		alert("hagan la tarea");
 		if(situacionSentimental!="Soltero" && edadIngresada>12)
 		{
 			alert("sos muy joven para no ser soltero");
 		}
+		if(edadIngresada<13)//niños
+		{
+			alert("hagan la tarea");
+		}
 	}else
 	{
-		if(edadIngresada>17 && edadIngresada<61)
+		if(edadIngresada>17)//mayores de edad
 		{
 			alert("se responsable");
+			if(edadIngresada>60)//adultos mayores
+			{
+				alert("sos persona de riesgo");
+			}
 			if(situacionSentimental=="Soltero")
 			{
 				alert("a vivir la vida");
@@ -45,64 +52,12 @@ function mostrar()
 				if(situacionSentimental=="Casado")
 				{
 					alert("a disfrutar la pareja");
-				}else
-				{
-					alert("a intentarlo nuevamente");
 				}
 			}
-		}else
-		{
-			alert("sos persona de riesgo");
 		}
 	}
-
-	
-	
-	
-	
-	
-	
-	
-	
-	/*if(edadIngresada<13)
+	if(situacionSentimental=="Divorciado")
 	{
-		alert("hagan la tarea");
+		alert("a intentarlo nuevamente");
 	}
-	else
-	{
-		if(edadIngresada>12 && edadIngresada<18)
-		{
-			alert("respeta a tus mayores");
-			if(situacionSentimental!="Soltero")
-			{
-				alert("sos muy joven para no ser soltero");
-			}
-		}
-		else
-		{
-			if(edadIngresada>17 && edadIngresada<61)
-			{
-				alert("se responsable");
-				if(situacionSentimental=="Soltero")
-				{
-					alert("a vivir la vida");
-				}
-				else
-				{
-					if(situacionSentimental=="Casado")
-					{
-						alert("a disfrutar la pareja");
-					}
-				}
-			}
-			else
-			{
-				alert("sos persona de riesgo");
-			}
-			if(situacionSentimental=="Divorciado")
-			{
-				alert("a intentarlo nuevamente");
-			}
-		}	
-	}*/
 }//FIN DE LA FUNCIÓN
