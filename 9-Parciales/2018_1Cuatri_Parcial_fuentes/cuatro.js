2c15b2f9851b36990bdccbd15c8dab4a73f2d983
JoseEnriquez884 <bishop015@hotmail.com>
2020-08-17 23:53:22 -0300

    repasando

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,41 @@
 function mostrar()
 {
+    var primerNumero;
+    var segundoNumero;
+    var suma;
+    var resta;
+
+    primerNumero=prompt("ingrese primer numero");
+    primerNumero=parseInt(primerNumero);
+
+
+    segundoNumero=prompt("ingrese segundo numero");
+    segundoNumero=parseInt(segundoNumero);
+
+    resta=primerNumero-segundoNumero;
+    suma=primerNumero+segundoNumero;
+
+    if(primerNumero==segundoNumero)
+    {
+        console.log(primerNumero+""+segundoNumero);
+    }else
+    {
+        if(primerNumero>segundoNumero)
+        {
+            resta;
+            console.log("resta: "+resta);
+        }else
+        {
+            suma;
+            console.log(suma);
+            if(suma>10)
+            {
+                console.log("la suma es "+suma+" y supero el 10");
+            }
+        }
+    }
+
+    
+
 
 }
